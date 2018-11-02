5a7e989848c13f8a226e98739fafc3e294363cd1
LeeSinLiang <38833175+LeeSinLiang@users.noreply.github.com>
2018-11-02 22:13:52 +0800

    initial commit

--- budget_project/settings.py
+++ budget_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 # import django.contrib.auth
 # django.contrib.auth.LOGIN_URL = '/'
 
@@ -120,7 +121,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/account/'
+
+django_heroku.settings(locals())
